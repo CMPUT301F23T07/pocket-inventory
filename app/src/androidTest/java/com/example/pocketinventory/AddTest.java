8ff004a0fbdb392b7bd3ce93bac9bb639659ed2d
Shikhararora19 <shikhar3@ualberta.ca>
2023-11-09 21:07:49 -0700

    Added test for longclick delete and longclick assign tags

--- app/src/androidTest/java/com/example/pocketinventory/AddTest.java
+++ app/src/androidTest/java/com/example/pocketinventory/AddTest.java
@@ -53,22 +53,36 @@ import java.util.Calendar;
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(year, monthOfYear, dayOfMonth));
         onView(withText("OK")).perform(click());
     }
+    private void fillInapple(){
+        // Fill in item details
+        Espresso.onView(ViewMatchers.withId(R.id.make_edit_text)).perform(ViewActions.typeText("Apple"));
+        Espresso.onView(ViewMatchers.withId(R.id.model_edit_text)).perform(ViewActions.typeText("iPhone"));
+        Espresso.onView(ViewMatchers.withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("1800"));
+        Espresso.onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("1234567890"));
+        Espresso.onView(ViewMatchers.withId(R.id.description_edit_text)).perform(ViewActions.typeText("xxxy"));
+        AddTest.setDate(R.id.date_of_purchase_edit_text, 2023, 9, 15);
+        Espresso.onView(withId(R.id.tag_edit_text)).perform(typeText("expensive, fast, durable"));
 
+    }
+    private void fillInasus(){
+        // Fill in item details
+        Espresso.onView(ViewMatchers.withId(R.id.make_edit_text)).perform(ViewActions.typeText("Asus"));
+        Espresso.onView(ViewMatchers.withId(R.id.model_edit_text)).perform(ViewActions.typeText("A15"));
+        Espresso.onView(ViewMatchers.withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("2000"));
+        Espresso.onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("3441412"));
+        Espresso.onView(ViewMatchers.withId(R.id.description_edit_text)).perform(ViewActions.typeText("asdad"));
+        AddTest.setDate(R.id.date_of_purchase_edit_text, 2020, 9, 15);
+        Espresso.onView(withId(R.id.tag_edit_text)).perform(typeText("expensive, fast, durable"));
+
+    }
+    //US 01.01.01,US 02.01.01,US 03.02.01
     @Test
     public void testAddItemAndCheckModel() {
         // Click the "Add Item" button
         Espresso.onView(ViewMatchers.withId(R.id.add_item)).perform(ViewActions.click());
 
         // Fill in item details
-        Espresso.onView(ViewMatchers.withId(R.id.make_edit_text)).perform(ViewActions.typeText("Apple"));
-        Espresso.onView(ViewMatchers.withId(R.id.model_edit_text)).perform(ViewActions.typeText("iPhone"));
-        Espresso.onView(ViewMatchers.withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("1800"));
-        Espresso.onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("1234567890"));
-
-        Espresso.onView(ViewMatchers.withId(R.id.description_edit_text)).perform(ViewActions.typeText("xxxy"));
-        AddTest.setDate(R.id.date_of_purchase_edit_text, 2023, 9, 15);
-        Espresso.onView(withId(R.id.tag_edit_text)).perform(typeText("expensive, fast, durable"));
-
+        fillInapple();
         Espresso.onView(ViewMatchers.withId(R.id.add_button)).perform(ViewActions.click());
 
     }
@@ -77,15 +91,7 @@ import java.util.Calendar;
     public void testdelete() {
         // Click the "Add Item" button
         Espresso.onView(ViewMatchers.withId(R.id.add_item)).perform(ViewActions.click());
-
-        // Fill in item details
-        Espresso.onView(ViewMatchers.withId(R.id.make_edit_text)).perform(ViewActions.typeText("Apple"));
-        Espresso.onView(ViewMatchers.withId(R.id.model_edit_text)).perform(ViewActions.typeText("iPhone"));
-        Espresso.onView(ViewMatchers.withId(R.id.estimated_value_edit_text)).perform(ViewActions.typeText("1800"));
-        Espresso.onView(withId(R.id.serial_number_edit_text)).perform(ViewActions.typeText("1234567890"));
-        Espresso.onView(ViewMatchers.withId(R.id.description_edit_text)).perform(ViewActions.typeText("xxxy"));
-        AddTest.setDate(R.id.date_of_purchase_edit_text, 2023, 9, 15);
-        Espresso.onView(withId(R.id.tag_edit_text)).perform(typeText("expensive, fast, durable"));
+        fillInapple();
 
         // Click the "Add" button to add the item
         Espresso.onView(ViewMatchers.withId(R.id.add_button)).perform(ViewActions.click());
@@ -102,6 +108,32 @@ import java.util.Calendar;
         Espresso.onView(ViewMatchers.withId(R.id.delete_icon)).perform(ViewActions.click());
 
         // Verify that the item is deleted or check any confirmation dialog
+    }
+
+    @Test
+    public void testAssigntagstoItems(){
+        Espresso.onView(ViewMatchers.withId(R.id.add_item)).perform(ViewActions.click());
+        fillInapple();
+
+        // Click the "Add" button to add the item
+        Espresso.onView(ViewMatchers.withId(R.id.add_button)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.add_item)).perform(ViewActions.click());
+        fillInasus();
+
+        // Click the "Add" button to add the item
+        onView(ViewMatchers.withId(R.id.add_button)).perform(ViewActions.click());
+
+        // Now, long click on the added item (replace R.id.recyclerViewId with your RecyclerView's ID)
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        onView(ViewMatchers.withId(R.id.log_list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, ViewActions.longClick()));
+        onView(ViewMatchers.withId(R.id.log_list)).perform(RecyclerViewActions.actionOnItemAtPosition(1, ViewActions.click()));
+        onView(ViewMatchers.withId(R.id.add_tag_icon)).perform(ViewActions.click());
+        onView(ViewMatchers.withId(R.id.tagSelectedEditText)).perform(ViewActions.typeText("battery, charger"));
+        onView(ViewMatchers.withText("CONFIRM")).perform(ViewActions.click());
     }
 }
 
