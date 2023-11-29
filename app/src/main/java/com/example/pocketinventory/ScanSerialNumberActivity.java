387e2650679fe801d074b84dc597e83c4b242ba9
Hanishakovvuru <122633032+Hanishakovvuru@users.noreply.github.com>
2023-11-28 17:18:32 -0700

    Added Javadoc comments

--- app/src/main/java/com/example/pocketinventory/ScanSerialNumberActivity.java
+++ app/src/main/java/com/example/pocketinventory/ScanSerialNumberActivity.java
@@ -9,12 +9,22 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+/**
+ * This class is the scan serial number activity of the app. It gives the user access to the camera to take
+ * photos of serial number and save the serial number in the item details
+ */
 public class ScanSerialNumberActivity extends AppCompatActivity {
+    /**
+     * Called when the activity is starting
+     *
+     * @param savedInstanceState If the activity is being re-initialized after previously being shut down,
+     *                           this Bundle contains the data it most recently supplied in onSaveInstanceState.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_serial_number);
-    /*
+
         Intent intent = getIntent();
         Item item = intent.getParcelableExtra("item");
 
@@ -46,6 +56,5 @@ public class ScanSerialNumberActivity extends AppCompatActivity {
 
         // Call the ActionMode
         ((AppCompatActivity) this).startActionMode(callback);
-    */
     }
 }
